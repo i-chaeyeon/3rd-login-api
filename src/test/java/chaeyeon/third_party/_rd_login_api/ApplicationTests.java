e1ddc793d5682ac543cfe195758454602d0b962d
i-chaeyeon <leechae0605@naver.com>
2025-06-28 14:08:33 +0900

    initial commit

---
+++ src/test/java/chaeyeon/third_party/_rd_login_api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package chaeyeon.third_party._rd_login_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
